b5b1669c09312eec93d40d17811e9e6c6378ec36
Wan Kim Mok <cafe.mui@gmail.com>
2014-11-13 23:07:46 -0500

    created function to create database

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,5 @@
 #all imports 
+from contextlib import closing
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
@@ -16,6 +17,12 @@ app.config.from_object(__name__)
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
 	app.run()
